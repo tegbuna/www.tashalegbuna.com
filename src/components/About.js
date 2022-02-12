91ced9f0ea68eb6faab68d6e8e7c3f7cff454928
Tasha Egbuna <tasha.egbuna@gmail.com>
2022-02-11 18:44:19 -0600

    formatting about

--- src/components/About.js
+++ src/components/About.js
@@ -13,13 +13,13 @@ const About = ({title}) => {
                 <h2>{title}</h2> 
                     {/* <img className="about-photo" src={Tasha} alt="Tasha L. Egbuna" /> */}
                   
-                      <p className="para-1"></p>  I'm a full stack software engineer with a huge passion for front-end  development.  I have a diverse background but where I have always exeled is connecting with people of all levels, solving problems and adding value for the greater good. A few years ago I became curious about coding and that led me to General Assembly's Software Engineering Immersive Program. 
+                      <p className="para-1"></p>  I'm a full stack software engineer with a huge passion for front-end  development.  A few years ago I became curious about coding and that led me to General Assembly's Software Engineering Immersive Program. 
                       <br /><br />
-                      <p className="para-2"></p>   As a developer I am eager to collaborate on innovative teams and I find great satisfaction in solving unique design  and programming challenges. As an engaging and skilled communicator in my previous profession, I  developed a successful portfolio, substantial peer and client networks, and a strong record of advancement and leadership.  
-           
-                      <br /><br />
-
-                      <p> In this endevor - as in all endevors, I am committed to continual learning, growth and success.`` I look forward to the opportunity to connect with you.</p>
+                      <p>I  had a diverse background prior to software development but I've always excelled at connecting with people of all levels.  I  developed a successful portfolio, substantial peer and client networks, and a strong record of advancement and leadership. </p> 
+                      <br />
+                      <p className="para-2">   As a developer I am eager to collaborate on innovative teams solving unique programming and design challenges  where I can add value for the greater good.  In this endevor - as in all endevors, I am committed to continual learning, growth and success.</p>  
+                      
+                      <br /><p>I look forward to connecting with you.</p>
                      
               </div>
 
