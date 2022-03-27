6019a3a94b6b5cbf85f04bc54fb61ab91205de3f
Tasha Egbuna <tasha.egbuna@gmail.com>
2022-03-27 13:13:15 -0500

    fix typo

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ const About = ({title}) => {
                         </div>
                         <div className="two"> 
                           <p>In every career prior to software development I have  excelled at connecting with people of all levels.  I  developed a successful portfolio, substantial peer and client networks, and a strong record of advancement and leadership. </p> 
-                          <p> As a developer I am eager to collaborate on innovative teams solving unique programming and design challenges  where I can add value for the greater good.  In this endevor - as in all endevors, I am committed to continual learning, growth and success and I look forward to connecting with you.</p> </div>  
+                          <p> As a developer I am eager to collaborate on innovative teams solving unique programming and design challenges  where I can add value for the greater good.  In this endeavor - as in all endeavors, I am committed to continual learning, growth and success and I look forward to connecting with you.</p> </div>  
                            <br />
                         </div> 
                       <div className="email">~tasha </div>
